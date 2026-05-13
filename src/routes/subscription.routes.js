e924c64789f471b80125d1df83909641658e75df
notsomohit <mohitascend07@gmail.com>
2026-05-14 04:29:12 +0530

    feat:add getAllSubscriptions and getUserSubscriptions

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { createSubscription } from "../controllers/susbscription.controller.js";
+import { createSubscription,getUserSubscription,getAllSubscriptions } from "../controllers/susbscription.controller.js";
 import { authorize } from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get("/",(req,res) => {
-    res.send({ title: "get all subscription"});
-});
+subscriptionRouter.get("/",authorize,getAllSubscriptions);
 
 subscriptionRouter.get("/:id",(req,res) => {
     res.send({ title: "get subscription details"});
@@ -22,9 +20,7 @@ subscriptionRouter.delete("/:id",(req,res) => {
     res.send({ title: "delete subscription"});
 });
 
-subscriptionRouter.get("/user/:id",(req,res) => {
-    res.send({ title: "get all user subscriptions"});
-});
+subscriptionRouter.get("/user/:id",authorize,getUserSubscription);
 
 subscriptionRouter.put("/:id/cancel",(req,res) => {
     res.send({ title: "cancel subscription"});
