ea9ce7b4945105e865f519f39baa09446c239f0b
notsomohit <mohitascend07@gmail.com>
2026-05-13 23:00:50 +0530

    feat: add authorization

--- config/env.js
+++ config/env.js
@@ -9,3 +9,7 @@ export const {
     JWT_SECRET,
     JWT_SECRET_EXPIRY
 } = process.env;
+
+//token -> eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2YTA0YTQ2MjViZTBhY2JkY2I3YWE1NTUiLCJpYXQiOjE3Nzg2ODkxMjIsImV4cCI6MTc3ODc3NTUyMn0.2ir4jNfbfIz9QmE3Wvq3HA0f4UcJZbVDr8f_g0hOJ2g 
+// //_id -> 6a04a4625be0acbdcb7aa555
+
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -24,17 +24,10 @@ export const signUp = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUsers = await User.create(
-            [{ name, email, password: hashedPassword }],
-            { session }
-        );
+        const newUsers = await User.create([{ name, email, password: hashedPassword }],{ session });
 
         // create token
-        const token = jwt.sign(
-            { userId: newUsers[0]._id },
-            JWT_SECRET,
-            { expiresIn: JWT_SECRET_EXPIRY }
-        );
+        const token = jwt.sign({ userId: newUsers[0]._id },JWT_SECRET,{ expiresIn: JWT_SECRET_EXPIRY });
 
         await session.commitTransaction();
         session.endSession();
@@ -56,6 +49,38 @@ export const signUp = async (req, res, next) => {
 };
 
 export const signIn = async (req, res, next) => {
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            const error = new Error("user not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            const error = new Error("password is not valid");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId:user._id},JWT_SECRET,{expiresIn:JWT_SECRET_EXPIRY});
+
+        res.status(200).json({
+            success: true,
+            message: "user signed in successfully",
+            data:{
+                token,
+                user
+            }
+        });
+
+    } catch (error) {
+        next(error);
+    }
 
 };
 
