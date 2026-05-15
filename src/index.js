8f2260bd65c9e5184857aff33f662f120bc4e50d
notsomohit <mohitascend07@gmail.com>
2026-05-16 00:27:35 +0530

    revamp

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import connectDB from './db/db.js';
-const port = process.env.PORT;
-
-app.listen(port,async()=>{
-    console.log(`you're listening to Subscription Tracker API on http://localhost:${port}`);
-    await connectDB();
-});
-
